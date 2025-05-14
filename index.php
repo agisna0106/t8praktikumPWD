ba35f306d13cff7b652f442bf722d5117136b366
agisna <agiiis0106@gmail.com>
2025-05-14 14:44:06 +0700

    data berhasil ter output

---
+++ index.php
@@ -0,0 +1,44 @@
+<?php
+require_once 'model/Kopi.php';
+
+$kopi = new Kopi();
+$table = 'kopi';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data Kopi</title>
+</head>
+<body>
+    <h1>Tabel User</h1>
+    <table border="1">
+        <thead>
+            <th>No</th>
+            <th>Nama</th>
+            <th>Stok</th>
+            <th>Harga</th>
+        </thead>
+        <tbody>
+            <?php
+                $no = 1;
+                $data = $kopi->tampil($table);
+                foreach ($data as $data) {
+                    echo '<tr>';
+                    echo '<td>'.$no++.'</td>';
+                    echo '<td>'.$data['nama'].'</td>';
+                    echo '<td>'.$data['stock'].'</td>';
+                    echo '<td>'.$data['harga'].'</td>';
+                    echo '</tr>';
+                }
+            ?>
+
+        </tbody>
+    </table>
+</body>
+</html>
+
+
+
